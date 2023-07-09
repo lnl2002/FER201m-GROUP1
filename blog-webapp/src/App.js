4814a5ec84f65e32cfd6f57d6b0f149a3257ee6c
icecute20302 <hungtthe163869@fpt.edu.vn>
2023-07-09 23:31:14 +0700

    userlist+update blogdetail

--- blog-webapp/src/App.js
+++ blog-webapp/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from '../src/screens/Home.js'
 import DetailBlog from './screens/DetailBlog';
+import UserList from './screens/UserList';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>} ></Route>
         <Route path='/blogs/detail/:pid' element={<DetailBlog/>} ></Route>
+        <Route path='userlist' element={<UserList/>} ></Route>
       </Routes>
     </BrowserRouter>
   );
